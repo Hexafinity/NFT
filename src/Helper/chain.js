1d9399cdc3f3e38c027a0d1850f31a58036d874b
DevWithReact <atroxltd@gmail.com>
2022-04-28 02:58:59 +0300

    build code structure

---
+++ src/Helper/chain.js
@@ -0,0 +1,16 @@
+export const chain_info = {
+    "56" : {
+        "symbol" : "BNB",
+        "chainId" : 56,
+        "ChainName" : "Binance Smart Chain Mainnet",
+        "rpcUrls" : "https://bsc-dataseed.binance.org/",
+        "nft_address":"0xe8feb8c889cb7296618b081c16d679840e76c585"
+    },
+    "97" : {
+        "symbol" : "TBNB",
+        "chainId" : 97,
+        "ChainName" : "Binance Smart Chain Testnet",
+        "rpcUrls" : "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        "nft_address":"0xC2cf2fc3A2D66fb36253250a05F849a7438fb80d"
+    }
+};
